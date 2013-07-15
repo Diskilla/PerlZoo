c15fdcfc4072aa7ca8ae9c06b8e8e494c5d2b3ba
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-07-15 12:05:31 +0200

    Ausgabe anpassen, damit die AttackPower dem richtigen Namen zugeordnet wird

--- Zoo.pl
+++ Zoo.pl
@@ -9,7 +9,7 @@ use Cat;
 
 my @pets;
 # create Dog Objects
-foreach my $name (qw( Butch Keagan Fluffy )) {
+foreach my $name (qw( Muecke Keagan Fluffy )) {
     push(@pets, Dog->New( Name => $name, AttackPower => undef ));
 }
 # create Cat Objects
@@ -17,17 +17,15 @@ foreach my $name (qw( Fritzi Smokey Mietze )) {
     push(@pets, Cat->New( Name => $name, AttackPower => 11 ));
 }
 
-my $garfield = Cat->New( Name => "Garfield", AttackPower => 12 );
-print $garfield->{Name};
-print( $garfield->get_AttackPower(), "\n" );
-print( $pets[0]->get_AttackPower(), "\n" );
-
 my $index = 0;
 
 while ( $index <= $#pets ) {
     foreach my $tierchen (@pets) {
-        print($pets[$index]->{Name}, "\n");
-        print($pets[$index]->get_AttackPower(), "\n");
+        print(  $pets[$index]->{Name},
+                " => ",
+                $pets[$index]->get_AttackPower(),
+                "\n"
+             );
         $index++;
     }
 }
